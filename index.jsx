4fabc0773debf2ec92c11355c7eb0613ab217148
Yusuf Zain Elabdin <130767740+Yusuf0Zain@users.noreply.github.com>
2025-07-28 22:19:28 +0800

    .

--- index.jsx
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-
-const content = `// Debug start file
-console.log('Debug mode started at', new Date().toISOString());
-`;
-
-fs.writeFileSync('debug-start.js', content);
-console.log('debug-start.js created.');
