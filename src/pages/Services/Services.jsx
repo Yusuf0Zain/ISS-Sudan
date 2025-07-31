599010302906d0fc51fe1fd3235c883b2d547344
Yusuf Zain Elabdin <130767740+Yusuf0Zain@users.noreply.github.com>
2025-08-01 00:15:44 +0800

    .

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import './Services.css';
+import faculty from '../../data/faculties.json';
 function Services() {
-  return <div>Services Page</div>;
+  return (
+    <div className="services-container">
+    {faculty.map((item) => (
+    <div className="button-container">
+      <button className="brutalist-button openai">
+        <div className="logo">
+          {item.icon && (
+            <img
+              src={item.icon}
+              className="icon"
+            />
+          )}
+        </div>
+        <div className="button-text">
+          <span>{item.name}</span>
+        </div>
+      </button>
+    </div>
+    ))}
+
+    </div>
+  );
 }
 
 export default Services;
