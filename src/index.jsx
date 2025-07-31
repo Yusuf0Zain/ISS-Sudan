5237a2c03df82b3cd90871f1e797a3634b528f85
Yusuf Zain Elabdin <130767740+Yusuf0Zain@users.noreply.github.com>
2025-07-31 15:40:46 +0800

    .

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,116 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import Navbar from './components/navbar.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
       <App />
       <div className="license">
         <img className="inline-block mr-2 align-middle logo-img" src="/logoW.png"/>
         <h1>ISS SUDAN UTM</h1>
         <p>Official Website for the International Student Society of Sudan at UTM</p>
         <p>© 2025 ISS-Sudan | All Rights Reserved</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>مالك نزلت تحت شديد؟</p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
       </div>
     </BrowserRouter>
   </StrictMode>,
